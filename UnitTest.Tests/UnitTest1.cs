ef393ec92ab56f739606381046469f7fd4ae4ea8
kakgonbri <buikhacbin@gmail.com>
2025-05-14 21:56:42 +0700

    Add project files.

---
+++ UnitTest.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+
+namespace UnitTest.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var result = Target.HellYeah();
+
+            result.Should().BeTrue();
+        }
+    }
+}
---
+++ UnitTest/Target.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    internal class Target
+    {
+        public static bool HellYeah()
+        {
+            return true;
+        }
+    }
+}
